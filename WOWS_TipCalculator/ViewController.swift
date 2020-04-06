206b0e3f991c65a12284d6df821c3ebe4d5d02a2
Theo Vora <theo.vora@gmail.com>
2020-04-06 14:19:57 -0600

    initial commit

---
+++ WOWS_TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WOWS_TipCalculator
+//
+//  Created by Theo Vora on 4/6/20.
+//  Copyright © 2020 Theo Vora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
