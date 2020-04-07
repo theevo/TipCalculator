23e0645468a84973bd65302f2d9df7e4d121c580
Theo Vora <theo.vora@gmail.com>
2020-04-06 20:08:25 -0600

    add TipCalculator controller
     Please enter the commit message for your changes. Lines starting

--- WOWS_TipCalculator/Controllers/TipCalculatorController.swift
+++ WOWS_TipCalculator/Controllers/TipCalculatorController.swift
@@ -8,19 +8,66 @@
 
 import Foundation
 
-class TipCalculator {
+class TipCalculatorController {
     
-    static let shared = TipCalculator()
+    // MARK: - Shared Instance
     
-    var tipDollarAmount: Float = 18.0
-    var tabAmount: Float = 100.0 {
+    static let shared = TipCalculatorController()
+    
+    
+    // MARK: - Properties
+    
+    var tipDollarAmountString: String = "$18.00"
+    var tabAmountString: String = "$100.00"
+    var tipPercentageString: String = "18%"
+    
+    private var tipDollarAmount: Double = 18.0 {
         didSet {
-            tipDollarAmount = tabAmount * tipPercentage
+            tipDollarAmountString = formatAsMoney(tipDollarAmount)
         }
     }
-    var tipPercentage: Float = 0.18 {
+    
+    private var tabAmount: Double = 100.0 {
         didSet {
             tipDollarAmount = tabAmount * tipPercentage
+            tabAmountString = formatAsMoney(tabAmount)
         }
     }
-}
+    
+    private var tipPercentage: Double = 0.18 {
+        didSet {
+            tipDollarAmount = tabAmount * tipPercentage
+            tipPercentageString = formatAsPercent(tipPercentage)
+        }
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+    func formatAsMoney(_ amount: Double) -> String {
+        let moneyFormatter = NumberFormatter()
+        moneyFormatter.numberStyle = NumberFormatter.Style.currency
+        return moneyFormatter.string(for: amount) ?? ""
+    }
+    
+    func formatAsPercent(_ amount: Double) -> String {
+        let percentFormatter = NumberFormatter()
+        percentFormatter.numberStyle = NumberFormatter.Style.percent
+        percentFormatter.maximumFractionDigits = 1
+        
+        return percentFormatter.string(for: amount) ?? ""
+    }
+    
+    func updateWithTipPercentString(string: String) {
+        guard let multiplier = Double(string.replacingOccurrences(of: "%", with: "")) else { return }
+        
+        tipPercentage = multiplier / 100.0
+    }
+    
+    func updateWithTabAmountString(string: String) {
+        guard let maybeTabAmount = Double(string.replacingOccurrences(of: "$", with: "")) else { return }
+        
+        tabAmount = maybeTabAmount
+    }
+    
+} // end class
