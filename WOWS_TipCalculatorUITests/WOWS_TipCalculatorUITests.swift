2f3314d51190bd14c81a7009790cff44de22590b
Theo Vora <theo.vora@gmail.com>
2020-06-02 21:45:16 -0600

    Add UI test that will change value of tab amount text field

--- WOWS_TipCalculatorUITests/WOWS_TipCalculatorUITests.swift
+++ WOWS_TipCalculatorUITests/WOWS_TipCalculatorUITests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-@testable import WOWS_TipCalculator
+//@testable import WOWS_TipCalculator
 
 class WOWS_TipCalculatorUITests: XCTestCase {
     
@@ -30,33 +30,64 @@ class WOWS_TipCalculatorUITests: XCTestCase {
         app = nil
     }
 
-    func testEditingTabAmount() throws {
+    func testTabAmountStartingValue() {
+        let tabAmountTextField = app.textFields["$100.00"]
+        
+        XCTAssertTrue(tabAmountTextField.exists, "Tab text field doesn't exist.")
+        
+    }
+    
+    func testNumPad() {
+        
         // 1. given
         
         let tabAmountTextField = app.textFields["$100.00"]
-//        let tabAmountField = app.segmentedControls.textFields["$100.00"]
+
+        tabAmountTextField.doubleTap()
+        
         let deleteKey = app/*@START_MENU_TOKEN@*/.keys["Delete"]/*[[".keyboards.keys[\"Delete\"]",".keys[\"Delete\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         let key = app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         let key2 = app/*@START_MENU_TOKEN@*/.keys["0"]/*[[".keyboards.keys[\"0\"]",".keys[\"0\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         let doneButton = app.toolbars["Toolbar"].buttons["Done"]
         
-        XCTAssertTrue(tabAmountTextField.exists, "Tab text field doesn't exist.")
         
         // 2. then
         
-        // double tap?
-        tabAmountTextField.tap()
-        tabAmountTextField.tap()
+        XCTAssertTrue(deleteKey.exists, "Delete key doesn't exist.")
+        XCTAssertTrue(key.exists, "Number 2 key doesn't exist.")
+        XCTAssertTrue(key2.exists, "Number 0 key doesn't exist.")
+        XCTAssertTrue(doneButton.exists, "Done button doesn't exist.")
+        
+    }
+        
+    func testChangeTabAmount() {
+        
+        // 1. given
+        
+        let tabAmountTextField = app.textFields["$100.00"]
+
+        tabAmountTextField.doubleTap()
+        
+        let deleteKey = app/*@START_MENU_TOKEN@*/.keys["Delete"]/*[[".keyboards.keys[\"Delete\"]",".keys[\"Delete\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let key = app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let key2 = app/*@START_MENU_TOKEN@*/.keys["0"]/*[[".keyboards.keys[\"0\"]",".keys[\"0\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let doneButton = app.toolbars["Toolbar"].buttons["Done"]
+        
+        // 2. where
         
         deleteKey.tap()
         
         key.tap() // 2
         key2.tap() // 0
         key2.tap() // 0
-        
+        expectation(for: NSPredicate(format: "hittable == true"), evaluatedWith: doneButton, handler: nil)
+        waitForExpectations(timeout: 15, handler: nil)
         doneButton.tap()
         
+        // 3. then
         
+        let newTabAmountTextField = app.textFields["$200.00"]
         
+        XCTAssertTrue(newTabAmountTextField.exists, "Didn't change value.")
     }
 }
